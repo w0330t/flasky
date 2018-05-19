df829391f6ff74f64cc81c92bea6f44e70f3a3df
Blueness <w0330t@gmail.com>
2018-05-19 22:59:37 +0800

    3

--- hello.py
+++ hello.py
@@ -1,43 +1,34 @@
-from flask import Flask, request, make_response, redirect, abort
-from flask_script import Manager
+from datetime import datetime
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
-manager = Manager(app)
+botstrap = Bootstrap(app)
+moment = Moment(app)
+
 
 @app.route('/')
 def index():
-    # return 'Hello World!'
-
-    # 返回UserAgent
-    # user_agent = request.headers.get('User-Agent')
-    # return '<p>Your browser is %s</p>' % user_agent
-
-    # 返回状态码
-    # return '<h1>Bad Request</h1>', 400
-
-    # 返回对象并设置cookie
-    # response = make_response('<h1>This document carries a cookie!</h1>')
-    # response.set_cookie('answer', '42')
-    # return response
-
-    # 重定向
-    return redirect('http://baidu.com')
-
-@app.route('/user/<id>')
-def get_user(id):
-    '''特殊的响应'''
-    # user = load_user(id)
-    user = None
-    if not user:
-        print('1111111111')
-        abort(404)
-    return '<h1>Hello, %s</h1>' %user
+    return render_template('index.html',
+                           current_time = datetime.utcnow())
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s</h1>' %name
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
-    # app.run()
-    manager.run()
+    app.run()
